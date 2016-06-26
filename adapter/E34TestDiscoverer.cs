94543f4c625cbb51dd495e5f104b88f504e9d5f4
freynaud <francois.reynaud@element34.net>
2016-06-26 20:28:46 +0200

    test discovery working?

---
+++ adapter/E34TestDiscoverer.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using WebDriverRunner;
+using WebDriverRunner.internals;
+
+namespace E34TestAdapter
+{
+    [DefaultExecutorUri(E34TestExecutor.ExecutorUriString)]
+    [FileExtension(".dll")]
+    // [FileExtension(".exe")]
+    public class E34TestDiscoverer : ITestDiscoverer
+    {
+        public static IMessageLogger LOGGER;
+
+        public void DiscoverTests(IEnumerable<string> sources
+            , IDiscoveryContext discoveryContext
+            , Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging.IMessageLogger logger
+            , ITestCaseDiscoverySink discoverySink)
+        {
+            LOGGER = logger;
+            GetTests(sources, discoverySink, logger);
+        }
+
+        public static IEnumerable<TestCase> GetTests(IEnumerable<string> sourceFiles,
+            ITestCaseDiscoverySink discoverySink, IMessageLogger logger)
+        {
+            var tests = new List<TestCase>();
+
+            if (logger != null)
+            {
+                foreach (var sourceFile in sourceFiles)
+                {
+                    logger.SendMessage(TestMessageLevel.Informational, "Working on dll: " + sourceFile);
+                }
+            }
+
+            foreach (var sourceFile in sourceFiles)
+            {
+                try
+                {
+                    Configuration config = new Configuration();
+                    config.Dlls.Add(sourceFile);
+                    Runner runner = new Runner(config);
+                    runner.LoadTests();
+                    List<TestMethodInstance> all = runner.GetAllTests();
+                    logger.SendMessage(TestMessageLevel.Informational, "tests found :" + all.Count);
+
+
+                    foreach (var test in all)
+                    {
+
+                       
+                        /*var testCase = new TestCase(test, E34TestExecutor.ExecutorUri, sourceFile)
+                        {
+                            CodeFilePath = sourceFile,
+                            
+                            DisplayName = "test:" + test + "(chrome)",
+                        };*/
+                        var testCase = new TestCase(test.ToString(), E34TestExecutor.ExecutorUri, sourceFile)
+                        {
+                            CodeFilePath = test.Attr.File,
+                            DisplayName = test.ToString(),
+                            LineNumber = test.Attr.Line
+
+                        };
+                        if (discoverySink != null)
+                        {
+                            discoverySink.SendTestCase(testCase);
+                        }
+                        tests.Add(testCase);
+                    }
+                }
+                catch (Exception e)
+                {
+                    logger.SendMessage(TestMessageLevel.Error, e.Message);
+                }
+                
+            }
+            return tests;
+        }
+    }
+}
---
+++ adapter/E34TestExecutor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using WebDriverRunner;
+using WebDriverRunner.internals;
+
+namespace E34TestAdapter
+{
+    [ExtensionUri(E34TestExecutor.ExecutorUriString)]
+    public class E34TestExecutor : ITestExecutor
+    {
+        private bool canceled = false;
+
+        public const string ExecutorUriString = "executor://e34executor/v2";
+        public static readonly Uri ExecutorUri = new Uri(E34TestExecutor.ExecutorUriString);
+
+
+        public void Cancel()
+        {
+            canceled = true;
+        }
+
+        public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            var tests = E34TestDiscoverer.GetTests(sources, null, null);
+            RunTests(tests, runContext, frameworkHandle);
+        }
+
+        private void Log(String message)
+        {
+            if (E34TestDiscoverer.LOGGER != null)
+            {
+                E34TestDiscoverer.LOGGER.SendMessage(TestMessageLevel.Informational, "E34TestExecutor::RunTests");
+            }
+        }
+
+        public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+        {
+            var olock = new Object();
+            var cache = new Dictionary<string, string>();
+
+            
+
+
+            foreach (var test in tests)
+            {
+                Log("E34TestExecutor::RunTests=" + test.Source);
+               
+                var result = new TestResult(test);
+                result.Outcome = TestOutcome.Failed;
+                result.ErrorMessage = "Not implemented."+test.Source+" -- "+test.CodeFilePath;
+                frameworkHandle.RecordResult(result);
+            }
+
+          
+
+
+            /* */
+        }
+    }
+}
